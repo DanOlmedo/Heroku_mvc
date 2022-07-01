5643c2b49b0f14b42e7f3aac8be234bb3d14c38f
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-07-01 12:26:17 -0500

    Added main view, crrected paths

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async (req,res) => {
-    res.render('dashboard')
+    res.render('all')
 })
 
 router.get('/login', async (req,res) => {
